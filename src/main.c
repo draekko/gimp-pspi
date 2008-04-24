44760ba842a63d506e5c67ab860bf74578d470de
Tor Lillqvist <tml@iki.fi>
2008-04-24 14:41:35 +0000

    2008-04-24  Tor Lillqvist  <tml@novell.com>
    	* COPYING
    	* src/*.[ch]: Relicense to "MIT License".
    
    	* README: Slightly more details about compiling on Linux.
    
    	* configure.in: Bump version to 1.0.7. Add ru to ALL_LINGUAS.
    
    	* src/main.c (query): Mark menu item blurb for translation.

--- src/main.c
+++ src/main.c
@@ -3,20 +3,26 @@
  *
  * Copyright (C) 2001 Tor Lillqvist
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include "config.h"
@@ -672,7 +678,7 @@ query (void)
   gimp_plugin_domain_register (PLUGIN_NAME, NULL);
 
   gimp_install_procedure (PSPI_SETTINGS_NAME,
-			  "Set the search path for pspi",
+			  N_("Set the search path for pspi"),
 			  "Set the directory trees where the pspi plug-in searches for Photoshop filter plug-ins",
 			  "Tor Lillqvist <tml@iki.fi>",
 			  "Tor Lillqvist <tml@iki.fi>",
